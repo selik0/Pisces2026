23969373b178819d286b4b070dcef269b02a98e8
selik0 <chenkantian@163.com>
2026-08-25 16:56:14 +0800

    ui管理

---
+++ GameProject/GameEngine/Runtime/UI/Component/UIRoot.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameEngine
+{
+    public class UIRoot : MonoBehaviour
+    {
+        public Camera UICamera;
+        public Canvas UICanvas;
+        public UnityEngine.EventSystems.EventSystem UIEventSystem;
+    }
+}
